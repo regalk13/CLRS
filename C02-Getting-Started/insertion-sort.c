c86801e338254441e2d597a74eeab86d8bb6dadb
regalk <jsalazarcaviedes30@gmail.com>
2023-04-06 19:56:54 -0500

    Add insertion sort non-increasing

--- C02-Getting-Started/insertion-sort.c
+++ C02-Getting-Started/insertion-sort.c
@@ -1,8 +1,5 @@
-int main(void) {
-
-  int A[] = {5, 2, 4, 6, 1, 3};
-
-  for (int j = 1; j < 6; j++) {
+void insertion_sort(int A[], int n) {
+  for (int j = 1; j < n; j++) {
       int key = A[j];
 
       int i = j - 1;
@@ -12,7 +9,38 @@ int main(void) {
           i -= 1;
       }
       A[i + 1] = key;
-  }
-  
-  return 0;  
+  } 
+}
+
+void decre_insertion_sort(int A[], int n) {
+  for (int j = 1; j < n; j++) {
+      int key = A[j];
+
+      int i = j - 1;
+
+      while (i >= 0 && A[i] < key) {
+          A[i + 1] = A[i];
+          i -= 1;
+      }
+      A[i + 1] = key;
+  } 
+}
+int main(void) {
+    int A[] = {5, 2, 4, 6, 1, 3};
+
+    insertion_sort(A, 6);
+
+    for (int i = 0; i < 6; i++) {
+        printf("Value in index[%d]: %d\n", i, A[i]);
+    }
+
+    printf("");
+        
+    decre_insertion_sort(A, 6);
+
+    for (int i = 0; i < 6; i++) {
+        printf("Value in index[%d]: %d\n", i, A[i]);
+    }
+    
+    return 0;  
 }
